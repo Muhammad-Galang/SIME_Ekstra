e243f1e3314669a12f42337692a79c3cbb69e583
Muhammad-Galang <rinsedosoy@gmail.com>
2023-10-03 10:43:22 +0700

    up server

--- app/config/Config.php
+++ app/config/Config.php
@@ -6,7 +6,7 @@ define('BASEURL', 'http://localhost/PROGRAM/Web-Sime%20-%20develop/public');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-// define('DB_NAME', 'dbsimanek');
+define('DB_NAME', 'dbsimanek');
 
 //branch renov_db,use:
-define('DB_NAME', 'dbsimanek_renov');
+// define('DB_NAME', 'dbsimanek_renov');
